7c827774fd9515b6ec55e84d1b871099c17f4092
Tanachok <scarmanpoom@gmail.com>
2022-07-06 23:44:19 +0700

    3.4-3.5 l

--- main.js
+++ main.js
@@ -2,17 +2,33 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
+            brand: 'SE 331',
             image: './assets/images/socks_green.jpg',
             inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
+            selectedVariant: 0,
             cart: 0
 
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        }
+    },
+    updateVariant(index) {
+        this.selectedVariant = index;
+    },
+    image() {
+        return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+        return this.variants[this.selectedVariant].quantity
     },
     methods: {
         addToCart() {
