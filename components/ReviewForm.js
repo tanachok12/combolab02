db0e680f3daea9816be47f4c22852ae62dbefde3
Tanachok <scarmanpoom@gmail.com>
2022-07-16 20:22:18 +0700

    6.8

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -27,7 +27,10 @@ app.component('review-form', {
     },
     methods: {
         onSubmit() {
-
+            if (this.name === '' || this.review === '' || this.rating === null) {
+                alert('Review is incomplete. Please fill out every field.')
+                return
+            }
             let productReview = {
                 name: this.name,
                 review: this.review,
