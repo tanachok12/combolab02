aee4c33aad81c44958ffefe52190ab5d9ff8a8df
Tanachok <scarmanpoom@gmail.com>
2022-07-16 19:30:27 +0700

    5.3 add

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const app = Vue.createApp({
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
             selectedVariant: 0,
-            cart: 0,
+            cart: [],
             onSale: true,
             premium: true
 
@@ -41,9 +41,9 @@ const app = Vue.createApp({
         updateImage(variantImage) {
             this.image = variantImage
         },
-        updateCart() {
-            this.cart += 1;
-        }
+        updateCart(id) {
+            this.cart.push(id);
+        },
     }
 
 })
