0e74a6ec8bc9193ee60fdab5324d9434331ee2e9
Tanachok <scarmanpoom@gmail.com>
2022-07-16 20:09:19 +0700

    6.2

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -4,12 +4,12 @@ app.component('review-form', {
         `<form class="review-form" @submit.prevent="onSubmit">
     <h3>Leave a review</h3>
     <label for="name">Name:</label>
-    <input id="name" v-model="name">
+    <input id="name" ">
     
     <label for="review">Review:</label>
-    <textarea id="review" v-model="review"></textarea>
+    <textarea id="review" "></textarea>
     <label for="rating">Rating:</label>
-    <select id="rating" v-model.number="rating">
+    <select id="rating"">
         <option>5</option>
         <option>4</option>
         <option>3</option>
